6a24f04cbd4dc0c7aa5e57083ae17e3dfb2c8d39
kbearl <kenneth.bearl@gmailcom>
2024-07-06 14:20:41 -0500

    project start

---
+++ src/main/java/com/mtg/mtg_rounds/MtgRoundsApplication.java
@@ -0,0 +1,13 @@
+package com.mtg.mtg_rounds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgRoundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgRoundsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtg/mtg_rounds/MtgRoundsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtg.mtg_rounds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgRoundsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
